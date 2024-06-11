dc58f73d4c7ff7e49cbaaa92737ea80e52689710
BrighTyso <brightkaponda96@gmail.com>
2024-06-11 17:22:29 +0200

    bale data

--- src/components/Capture.jsx
+++ src/components/Capture.jsx
@@ -4,6 +4,55 @@ import './balereceiving.css'
 
 export const Capture = (props) => {
 
+    let ticketid=0;
+
+    const getBarcodeData=()=>{
+
+        const barcode = document.getElementById("barcode").value
+
+
+        if (barcode.length===10) {
+            const d = new Date();
+            let date = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate()
+
+            const requestOptions = {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    userid: 1,
+                    barcode: barcode
+                })
+            };
+
+
+            fetch('http://localhost/king/api/get_barcode_data.php', requestOptions)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data[0])
+
+                    if (data.length>0){
+
+                         document.getElementById("grower_num").innerText=data[0].grower_num
+                         document.getElementById("group").innerText=data[0].group
+                         document.getElementById("lot").innerText=data[0].lot
+                         document.getElementById("mass").innerText=data[0].mass
+                         document.getElementById("ticketid").value=data[0].ticketid
+
+
+                    }else {
+                        document.getElementById("grower_num").innerText=""
+                        document.getElementById("group").innerText=""
+                        document.getElementById("lot").innerText=""
+                        document.getElementById("mass").innerText=""
+                        document.getElementById("ticketid").value=""
+                    }
+
+                });
+        }else {
+           // document.getElementById("response").innerText = " Capture Barcode"
+        }
+
+    }
 
     const saveBaleData=()=>{
 
@@ -75,30 +124,30 @@ export const Capture = (props) => {
                     </div> */}
                     <form action="">
                         <div>
-                            <label htmlFor="">Scan Barcode</label>
-                            <input type="barcode" className='form-control' id="barcode"/>
+                            <label htmlFor="" >Scan Barcode</label>
+                            <input type="barcode" className='form-control' id="barcode" onInput={getBarcodeData}/>
                         </div>
                         <br />
                         <div className='row'>
                             <div className='col'>
                                 <label htmlFor="">Grower</label>
-                              <p className='form-control' id=''></p>
+                              <p className='form-control' id='grower_num'></p>
 
                             </div>
                             <div className='col'>
                                 <label htmlFor="">Mass</label>
-                                <p className='form-control' id=''></p>
+                                <p className='form-control' id='mass'></p>
                             </div>
                         </div>
                         <div className='row'>
                             <div className='col'>
                                 <label htmlFor="">Group</label>
-                              <p className='form-control' id=''></p>
+                              <p className='form-control' id='group'></p>
 
                             </div>
                             <div className='col'>
                                 <label htmlFor="">Lot</label>
-                                <p className='form-control' id=''></p>
+                                <p className='form-control' id='lot'></p>
                             </div>
                         </div>
 
@@ -107,7 +156,7 @@ export const Capture = (props) => {
                         <div className='row'>
                             <div className='col'>
                                 <label htmlFor="">Sale Code</label>
-                              <select className='form-control' id=''>
+                              <select className='form-control' id='sale_code'>
                                 <option value="">Select</option>
                                 <option value="">RO</option>
                                 <option value="">CO</option>
@@ -118,19 +167,23 @@ export const Capture = (props) => {
                             </div>
                             <div className='col'>
                                 <label htmlFor="">TIMB Grade</label>
-                                <input className='form-control' id=''/>
+                                <input className='form-control' id='timb_grade'/>
                             </div>
                         </div>
                         <div className='row'>
                             <div className='col'>
                                 <label htmlFor="">Buyer Grade</label>
-                                <input className='form-control' id=''/>
+                                <input className='form-control' id='buyer_grade'/>
 
                             </div>
                             <div className='col'>
                                 <label htmlFor="">Price</label>
-                                <input className='form-control' id=''/>
+                                <input className='form-control' id='price'/>
                             </div>
+                        </div>
+
+                        <div>
+                            <label id="ticketid"></label>
                         </div>
                         
 
