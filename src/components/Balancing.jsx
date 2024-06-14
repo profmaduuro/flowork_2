fedaf05eb4c4a8ac53074a950f2061dd600e315b
BrighTyso <brightkaponda96@gmail.com>
2024-06-14 07:57:13 +0200

    balancing

--- src/components/Balancing.jsx
+++ src/components/Balancing.jsx
@@ -1,6 +1,125 @@
 import React from 'react'
 
 export const Balancing = (props) => {
+
+    const getBales=(event)=>{
+
+        console.log(event.target.id,"ok id")
+
+        const mySplit_transporter_growers_ids=event.target.id
+
+
+
+
+        //const id=document.getElementById("id").value
+        const splitid=mySplit_transporter_growers_ids.split(",")[0]
+        const transporter_growersid=mySplit_transporter_growers_ids.split(",")[1]
+
+
+
+        var x = document.getElementById("tbody1");
+
+
+        var tr =null;
+
+        var barcode=null;
+        var group=null;
+        var lot=null;
+        var mass=null;
+        var buyer_grade= null;
+        var timb_grade= null;
+        var price= null;
+
+        var sale_code= null;
+
+        while (x.hasChildNodes()) {
+            x.removeChild(x.firstChild);
+        }
+
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                transporter_growersid: transporter_growersid,
+                splitid: splitid,
+                userid: 1
+            })
+        };
+
+
+        fetch('http://localhost/king/api/get_bales_for_processing.php', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data,"my new data")
+
+
+                let count=0
+
+                // eslint-disable-next-line array-callback-return
+                data.map((u, i) => {
+
+                    console.log(data[i], "my new data")
+                    console.log(count, "count")
+
+                    // console.log(u,"data")
+                    let tr = document.createElement("tr");
+
+                    console.log(tr.key,"key")
+                    if (data.length>=count){
+
+
+
+                    barcode = document.createElement("td");
+                    sale_code = document.createElement("td");
+                    group = document.createElement("td");
+                    lot = document.createElement("td");
+                    buyer_grade = document.createElement("td");
+                    timb_grade = document.createElement("td");
+                    price = document.createElement("td");
+                    mass = document.createElement("td");
+
+                    var barcodeData = document.createTextNode(data[i].barcode);
+                    var groupData = document.createTextNode(data[i].bale_group);
+                    var lotData = document.createTextNode(data[i].lot);
+                    var massData = document.createTextNode(data[i].mass);
+
+                    var timb_gradeData = document.createTextNode(data[i].timb_grade);
+                    var buyer_gradeData = document.createTextNode(data[i].buyer_grade);
+                    var priceData = document.createTextNode(data[i].price);
+                    var sale_codeData = document.createTextNode(data[i].sale_code);
+
+                    barcode.appendChild(barcodeData)
+                    group.appendChild(groupData)
+                    lot.appendChild(lotData)
+                    sale_code.appendChild(sale_codeData)
+                    buyer_grade.appendChild(buyer_gradeData)
+                    timb_grade.appendChild(timb_gradeData)
+                    price.appendChild(priceData)
+                    mass.appendChild(massData)
+
+                    tr.appendChild(barcode)
+                    tr.appendChild(sale_code)
+                    tr.appendChild(group)
+                    tr.appendChild(lot)
+                    tr.appendChild(buyer_grade)
+                    tr.appendChild(timb_grade)
+                    tr.appendChild(price)
+                    tr.appendChild(mass)
+
+                    x.appendChild(tr)
+                    }
+
+                    count++
+                })
+
+            });
+
+
+
+    }
+
+
   return (
     <div>
      
@@ -24,34 +143,60 @@ export const Balancing = (props) => {
                         <th>Sale Date</th>
                         <th>ID</th>
                         <th>Batch</th>
-                        <th>USD Value</th>
-                        <th>ZIG value</th>
-                        <th>USD Paid</th>
-                        <th>ZIG Paid</th>
-                        <th>Booking</th>
-                        <th>Status</th>
+                        <th>Bales</th>
+                        <th>GrowerId</th>
+                        <th>Balanced</th>
+
                     </thead>
                     <tbody>
 
                     {
+
+
                         props.balancingData.map((u,i)=>{
-                            return(
-                                <tr  data-bs-toggle="modal" data-bs-target="#example2">
-                                    <td>{props.balancingData[i].grower_num}</td>
-                                    <td>{props.balancingData[i].name +" "+props.balancingData[i].surname}</td>
-                                    <td>{props.balancingData[i].splitid}</td>
-                                    <td>VLT</td>
-                                    <td>15-05-2024</td>
-                                    <td>{props.balancingData[i].transporter_growersid}</td>
-                                    <td>0001</td>
-                                    <td>1500</td>
-                                    <td>15000</td>
-                                    <td>1200</td>
-                                    <td>12000</td>
-                                    <td>Yes</td>
-                                    <td>Captured</td>
-                                </tr>
-                            )
+                            console.log(props.balancingData);
+                            let id=[]
+                            id.push(props.balancingData[i].splitid)
+                            id.push(props.balancingData[i].transporter_growersid)
+
+                            if (props.balancingData[i].already_balanced==="YES"){
+                                return(
+                                    <tr   id={id} >
+                                        <td>{props.balancingData[i].grower_num}</td>
+                                        <td>{props.balancingData[i].name +" "+props.balancingData[i].surname}</td>
+                                        <td>{props.balancingData[i].splitid}</td>
+                                        <td>{props.balancingData[i].selling_point_name}</td>
+                                        <td>15-05-2024</td>
+                                        <td>{props.balancingData[i].transporter_growersid}</td>
+                                        <td>0001</td>
+                                        <td>{props.balancingData[i].number_of_bales}</td>
+                                        <td>{props.balancingData[i].growerid}</td>
+                                        <td>{props.balancingData[i].already_balanced}</td>
+                                        <td><a href="" data-bs-toggle="modal" data-bs-target="#example2"  id={id} value={id} onClick={getBales}>Tickets Confirmed</a></td>
+
+                                    </tr>
+                                )
+                            }else {
+                                return(
+
+
+                                    <tr   id={id} >
+                                        <td>{props.balancingData[i].grower_num}</td>
+                                        <td>{props.balancingData[i].name +" "+props.balancingData[i].surname}</td>
+                                        <td>{props.balancingData[i].splitid}</td>
+                                        <td>{props.balancingData[i].selling_point_name}</td>
+                                        <td>15-05-2024</td>
+                                        <td>{props.balancingData[i].transporter_growersid}</td>
+                                        <td>0001</td>
+                                        <td>{props.balancingData[i].number_of_bales}</td>
+                                        <td>{props.balancingData[i].growerid}</td>
+                                        <td>{props.balancingData[i].already_balanced}</td>
+                                        <td><a href="" data-bs-toggle="modal" data-bs-target="#example2"  id={id} value={id} onClick={getBales}>Confirm Tickets</a></td>
+
+                                    </tr>
+                                )
+                            }
+
                         })
 
                         //
@@ -93,27 +238,7 @@ export const Balancing = (props) => {
                                     <th>Mass</th>
                                     </tr>
                                 </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>650002546R</td>
-                                        <td>RO <input type="text" className='col-2'/></td>
-                                        <td>5</td>
-                                        <td>4</td>
-                                        <td>LLOBS <input type="text" className='col-3'/> </td>
-                                        <td>L2LJ <input type="text" className='col-3'/></td>
-                                        <td>250 <input type="text" className='col-2'/></td>
-                                        <td>25</td>
-                                    </tr>
-                                    <tr>
-                                        <td>650002546R</td>
-                                        <td>RO</td>
-                                        <td>5</td>
-                                        <td>4</td>
-                                         <td>LLOBS</td>
-                                         <td>L2LJ</td>
-                                        <td>250</td>
-                                        <td>25</td>
-                                    </tr>
+                                <tbody id="tbody1">
 
                                     <tr>
                                         <td>650002546R</td>
@@ -135,28 +260,8 @@ export const Balancing = (props) => {
                                         <td>250</td>
                                         <td>25</td>
                                     </tr>
-                                    <tr>
-                                        <td>650002546R</td>
-                                        <td>RO <input type="text" className='col-2'/></td>
-                                        <td>5</td>
-                                        <td>4</td>
-                                        <td>LLOBS <input type="text" className='col-3'/> </td>
-                                        <td>L2LJ <input type="text" className='col-3'/></td>
-                                        <td>250 <input type="text" className='col-2'/></td>
-                                        <td>25</td>
-                                    </tr>
-                                    <tr>
-                                        <td>650002546R</td>
-                                        <td>RO</td>
-                                        <td>5</td>
-                                        <td>4</td>
-                                         <td>LLOBS</td>
-                                         <td>L2LJ</td>
-                                        <td>250</td>
-                                        <td>25</td>
-                                    </tr>
 
-                                   
+
                                 </tbody>
 
                             </table>
